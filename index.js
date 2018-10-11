3160af9001306d922e3125167e9f4cc80422ea11
Your Name <you@example.com>
2018-10-11 16:34:49 +0400

    avelacvac en /google,/search,/* uxiner

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ app.get("/", function(req, res){
     res.send("Hello world");
 });
 
+app.get("/name/:name", function(req, res){
+   var name = req.params.name;
+   res.send("<h1>Hello " + name + "</h1>");
+});
+
+app.get("/google", function(req, res){
+	var google = req.params.google
+	res.send("<a href='https://www.google.com/'>" + "google" + "</a>");
+});
+
+app.get("/search", function(req, res){
+	var search = req.params.search
+	res.send("<a href='https://www.google.com/search?q=search value'>" + "search" + "</a>");
+});
+
+app.get("/*", function(req, res){
+    res.send("404");
+});
+
 app.listen(3000, function(){
     console.log("Example is running on port 3000");
 });
