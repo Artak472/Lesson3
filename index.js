310e7da299db1b32a05d3661fcee123f47042976
Your Name <you@example.com>
2018-10-14 13:41:35 +0400

    popoxucyunner faylerum

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* console.log("Hello Node");
+/* 
+console.log("Hello Node");
 var os = require("os");
 var message = "The platform is ";
 
@@ -9,6 +10,7 @@ function main(){
 main();
 */
 
+/*
 var express = require("express");
 var app = express();
 
@@ -38,3 +40,79 @@ app.get("/*", function(req, res){
 app.listen(3000, function(){
     console.log("Example is running on port 3000");
 });
+*/
+
+/*
+var Square = require("./module");
+var mySquareObject = new Square(5);
+
+function main() {
+   console.log(mySquareObject.getArea());
+}
+main();
+*/
+
+/*
+var fs = require('fs');
+
+function main(){
+    var file = "hello.txt";
+    fs.appendFileSync(file, "Hello world\n");
+}
+main();
+*/
+
+/*
+var fs = require('fs');
+
+function main(){
+   fs.writeFile("hello.txt", "Hello world\n", function(err){
+       console.log("fs.writeFile ended");
+   });
+   console.log("fs.writeFile");
+}
+main();
+*/
+
+/*
+var fs = require('fs');
+
+function main() {
+   fs.writeFileSync("hello.txt", "Hello world\n");
+   console.log("fs.writeFile");
+}
+main();
+*/
+
+/*
+var fs = require('fs');
+var dummyText = "Apple yep";
+
+function main(){
+    fs.writeFileSync("dummytext.txt", dummyText);
+    var text = fs.readFileSync("dummytext.txt").toString();
+    console.log(dummyText == text);
+    console.log(text);
+    fs.writeFileSync("undummytext.txt",
+        text.replace("Apple", "Microsoft")
+    );
+}
+main();
+*/
+
+var obj = {
+    "first_name": "Artak",
+    "last_name": "Engibaryan",
+    "age": 17,
+    "tumo_student": true
+}
+
+var myJSON = JSON.stringify(obj);
+
+var fs = require('fs');
+
+function main() {
+   fs.writeFileSync("obj.json", myJSON);
+   console.log("fs.writeFile");
+}
+main();
